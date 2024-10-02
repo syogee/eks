219fbd13ae77ef6319335e372d52b706a8d321ab
YOGESH S <microgenesis.sm.s@mercedes-benz.com>
2024-10-02 22:49:52 +0530

    sh file to install aws-cli kubectl and terraform

--- installation.sh
+++ installation.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-set -e
 sudo apt-get update
-sudo apt-get install wget curl unzip
-TEMP_DIR="/tmp/aws-cli"
-mkdir -p "$TEMP_DIR"
-cd "$TEMP_DIR"
+sudo apt-get install -y wget curl unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
-sudo apt install terraform
+sudo apt install -y terraform
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-rm -rf awscliv2.zip aws
-echo "AWS CLI installation completed."
